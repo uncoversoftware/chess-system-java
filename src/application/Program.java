f3c78a72ec9ba4f531462a8bd7a6e2790291bf33
Uncover Software <uncoversoftware@gmail.com>
2020-01-15 08:52:15 -0300

    Atualização

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,51 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) 
 	{
+		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch  chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while (true)
+		{
+			
+		try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Soruce: ");
+				ChessPosition source = UI.readChessPossition(sc);
+				
+				System.out.println();
+				
+				System.out.print("Target : ");
+				ChessPosition target = UI.readChessPossition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
+		catch(ChessException e)
+		{
+			System.out.print(e.getMessage());
+			sc.nextLine();
+		}
+		catch(InputMismatchException e)
+		{
+			System.out.print(e.getMessage());
+			sc.nextLine();
+		}
+		
+		
+		}
 	}
 
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
@@ -26,20 +30,42 @@ public class UI {
 		
 	
 	
+	
+	public static void clearScreen()
+	{
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+	
+	
+	
+	public static ChessPosition readChessPossition(Scanner sc)
+	{
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		}
+		catch (RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+		}
+	}		
+	
+	
+	
+	
 	public static void printBoard(ChessPiece[][] pieces)
 	{
 		
-		for (int i=0;i<pieces.length;i++)
-		{
-			System.out.print((8-i) + " ");
-			for (int j = 0;j<pieces.length;j++)
-			{
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
-		
 	}
 	
 	
@@ -57,10 +83,7 @@ public class UI {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
-        System.out.print(" ");
-			
-		
-		System.out.print(" ");	
+        System.out.print(" ");	
 	}
 	
 	
