fe99ac2ff22181cf93e34461720852f037ce550c
Uncover Software <uncoversoftware@gmail.com>
2020-01-15 15:22:46 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,7 +7,9 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
    
 	private Color color;
-
+	private int moveCount;
+	
+	
 	public ChessPiece(Board board, chess.Color color) {
 		super(board);
 		this.color = color;
@@ -16,6 +18,20 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
+	
+	public void increaseMoveCount() {
+		this.moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		this.moveCount--;
+	}
+	
+	public int getMoveCount() {
+		return this.moveCount;
+	}
+
+
 	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
