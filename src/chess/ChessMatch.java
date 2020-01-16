c09c77fcc6550e0cc60ac45dffde856013d8b9b4
Uncover Software <uncoversoftware@gmail.com>
2020-01-16 08:40:18 -0300

    Bishop

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -229,10 +230,18 @@ public class ChessMatch {
   
 	private void initialSetup()
 	   {
-		   placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+		   //King.
 		   placeNewPiece('e', 1, new King(board, Color.WHITE));
+		   
+		   //Rook
+		   placeNewPiece('a', 1, new Rook(board, Color.WHITE));
 		   placeNewPiece('h', 1, new Rook(board, Color.WHITE));
 		   
+		   //Bishop.
+		   placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+		   placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+		   
+		   //Pawn.
 		   placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
 		   placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
 		   placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
@@ -242,10 +251,18 @@ public class ChessMatch {
 		   placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
 		   placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 		  
-		   placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+		   //King.
 		   placeNewPiece('e', 8, new King(board, Color.BLACK));
+		   
+		   //Rook.
+		   placeNewPiece('a', 8, new Rook(board, Color.BLACK));
 		   placeNewPiece('h', 8, new Rook(board, Color.BLACK));
 		   
+		   //Bishop.
+		   placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+		   placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+		   
+		   //Pawn.
 		   placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
 		   placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
 		   placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
